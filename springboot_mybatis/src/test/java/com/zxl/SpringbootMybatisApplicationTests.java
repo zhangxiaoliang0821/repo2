212968076710e5f8e98a5c150b8f9abc1d10c039
zxl <1131676434@qq.com>
2023-03-05 21:24:43 +0800

    2月第4周作业

---
+++ springboot_mybatis/src/test/java/com/zxl/SpringbootMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxl;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
