44134e0bf5dbcf53c0f9cd0a0893772d62cfd4d1
zxl <1131676434@qq.com>
2023-07-24 17:17:48 +0800

    demo
    demo

---
+++ demo_one/src/main/java/com/zxl/demo_one/DemoOneApplication.java
@@ -0,0 +1,13 @@
+package com.zxl.demo_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoOneApplication.class, args);
+    }
+
+}
---
+++ demo_one/src/test/java/com/zxl/demo_one/DemoOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxl.demo_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
